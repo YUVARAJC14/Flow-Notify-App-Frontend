692e1ca4c25acccb7e8e87f3af6c2287ab113ec5
Shakthithiru <shakthithiru04@gmail.com>
2025-09-11 09:37:02 +0530

    Flow-Notify-0

---
+++ FlowNotify3/app/src/main/java/com/saveetha/flownotify/network/ApiClient.kt
@@ -0,0 +1,30 @@
+package com.saveetha.flownotify.network
+
+import android.content.Context
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private const val BASE_URL = "http://10.0.2.2:8000/"
+
+    private var apiService: ApiService? = null
+
+    fun getInstance(context: Context): ApiService {
+        if (apiService == null) {
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(AuthInterceptor(context.applicationContext))
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            apiService = retrofit.create(ApiService::class.java)
+        }
+        return apiService!!
+    }
+}
